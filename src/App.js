61582c7f207e14773f9fe1e9d523d97e0ff120ab
Fikri Karim <fkfikrikarim@gmail.com>
2020-04-13 16:16:22 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
-        </div>
-        <div class="px-6 py-4">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #photography
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #travel
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-            #winter
-          </span>
+    <div class="px-5 md:px-10 pt-5">
+      <Header></Header>
+
+      <div class="pt-10">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+              #photography
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+              #travel
+            </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import image from "../images/header.svg";
+
+const Header = () => {
+  return (
+    <div class="">
+      <h2 class="font-semibold text-2xl md:text-3xl text-gray-700">
+        <a href="/">Pivots.dev</a>
+      </h2>
+
+      <section class="md:flex items-center justify-center h-64">
+        <div class="mx-auto items-center">
+          <div class="w-full md:w-10/12">
+            <div class="px-4">
+              <p class="md:font-semibold mt-4 text-2xl md:text-4xl text-gray-700">
+                Curated list of companies that had successful
+                <span class="text-blue-500"> pivots</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <img class="mt-10 b-auto sm:w-6/12 px-4" src={image}></img>
+      </section>
+    </div>
+  );
+};
+
+export default Header;
